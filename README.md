a3fef5564fec87be21d05b0ef98905df63600e3e
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2022-09-25 12:56:13 -0400

    setting up  API

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # The-Movie-DB-Michaelplant2
- 
+
+In this portfolio project, I built a single page app (SPA) with searchable movie listings using [The Movie Database Api (TMDB)](https://www.themoviedb.org/documentation/api).
+
+The desktop mockup has 2 main parts: a header and a main.
--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-// Example of api call. Get your api key from https://www.themoviedb.org/ and replace it with the one below. 
-// See https://developers.themoviedb.org/3/getting-started/introduction for more info
-const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=YOUR_API_KEY";
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d443cc02330f81935fbd017507b99041&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=YOUR_API_KEY&query=";
+// const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=YOUR_API_KEY&query=";
+
+const main = document.getElementById('main');
+
+getMovie(APIURL);
+
+async function getMovie(url) {
+  const resp = await fetch(url);
+  const respData = await resp.json();
+  console.log(respData);
+
+  showMovies(respData.results);
+}
 
 
-// Starter example of api properties.
 function showMovies(movies) {
   movies.forEach((movie) => {
-    const { poster_path, title, vote_average, overview } = movie;
+    const {poster_path, title, vote_average, overview} = movie;
+
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.innerHTML=
   });
 }
